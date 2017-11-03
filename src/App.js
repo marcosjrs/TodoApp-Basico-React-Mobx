22dd44a4c2c0cf9bbc22bb5cc48ebbef7355b302
marcosjrs <marcosjrs80@gmail.com>
2017-11-03 17:52:52 +0100

    CSS y Cambios "DOM". Maqueta de como quedaría...

--- src/App.js
+++ src/App.js
@@ -4,8 +4,19 @@ import { observer } from 'mobx-react'
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        App con ReactJS y Mobx
+      <div className="container">
+        <div className="row title">
+          <h1>Lista de "ToDo" con ReactJS y Mobx</h1>
+        </div>
+        <div className="row">
+            <div className="input-group input-group-todo">
+              <input type="text" className="form-control" placeholder="Añade tu tarea..."></input>
+            </div>        
+            <ul className="list-group list-group-todo">
+              <li className="list-group-item">Tarea uno</li>
+              <li className="list-group-item">Tarea dos</li>
+            </ul>
+        </div>        
       </div>
     );
   }
